ea202751857bbb887ff75c0289ebfb1f30c4e943
mskaa3 <j.moskaaa@gmail.com>
2022-10-21 08:02:53 +0200

    nitial version of qa and forum

---
+++ frontend/src/components/QAform.js
@@ -0,0 +1,25 @@
+/* eslint-disable prettier/prettier */
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function BasicExample() {
+  return (
+    <Form>
+     <center>
+     <div class="col-sm-6">
+      <Form.Group className="mb-3" controlId="formQuestion">
+        <Form.Label>Have any questions? We will try to answer them!</Form.Label>
+        <Form.Control type="email" placeholder="Type your question..." />
+
+      </Form.Group>
+    </div>
+    
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+       </center>
+    </Form>
+  );
+}
+
+export default BasicExample;
