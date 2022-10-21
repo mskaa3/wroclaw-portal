2472aa8cde1c22ed081d2059cca3b90e5169d142
WhiteOwlhead <86147489+WhiteOwlhead@users.noreply.github.com>
2022-10-21 09:21:28 +0200

    qa mockups + forum fix

--- frontend/src/screens/ForumScreen.js
+++ frontend/src/screens/ForumScreen.js
@@ -1,7 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import ForumTopics from '../components/ForumTopics';
-<<<<<<< HEAD
 
 
 const ForumScreen = () => {
@@ -10,15 +9,6 @@ const ForumScreen = () => {
       <ForumTopics />
     </div>
     )
-=======
-
-const ForumScreen = () => {
-  return (
-    <div>
-      <ForumTopics/>
-    </div>
-  );
->>>>>>> b83151e7a7205ca4b97bd1b2025d4aeac075711b
 };
 
 export default ForumScreen;
