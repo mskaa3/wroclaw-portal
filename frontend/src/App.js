81270c75e224b3c45175b07cebdd14ab438b5e84
Maryna Bastava <nik-vodo@yandex.ru>
2022-10-24 00:58:45 +0200

    initial version of front-back connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
+import axios from 'axios';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
@@ -13,8 +14,43 @@ import MapScreen from './screens/MapScreen';
 import NewsScreen from './screens/NewsScreen';
 import QaScreen from './screens/QaScreen';
 import LoginScreen from './screens/LoginScreen';
+import { useState, useEffect } from 'react';
+//import Univercity from './components/Univercity';
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000';
 
 const App = () => {
+  const [uniSearchWord, setUniSearchWord] = useState('');
+  const [unis, setUnis] = useState([]);
+
+  const handleSearchUni = async (e) => {
+    e.preventDefault();
+    console.log(uniSearchWord);
+
+    // fetch()
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     setUnis([data, ...unis]);
+    //     console.log(unis);
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //   });
+
+    try {
+      const res = await axios.get(`${API_URL}/unis`);
+      console.log([res.data]);
+      //setUnis([res.data, ...unis]);
+      setUnis(res.data || []);
+    } catch (error) {
+      console.log(error);
+    }
+
+    setUniSearchWord('');
+  };
+
+  //console.log(uniSearchWord);
+
   return (
     <Router>
       <HomeScreen />
@@ -24,7 +60,19 @@ const App = () => {
         <Container>
           <Routes>
             <Route exact path="/currency" element={<CurrencyScreen />} />
-            <Route exact path="/uni" element={<UniversityScreen />} />
+            <Route
+              exact
+              path="/uni"
+              element={
+                <UniversityScreen
+                  handleSubmit={handleSearchUni}
+                  uniSearchWord={uniSearchWord}
+                  setUniSearchWord={setUniSearchWord}
+                  unis={unis}
+                  setUnis={setUnis}
+                />
+              }
+            />
             <Route exact path="/news" element={<NewsScreen />} />
             <Route exact path="/map" element={<MapScreen />} />
             <Route exact path="/forum" element={<ForumScreen />} />
