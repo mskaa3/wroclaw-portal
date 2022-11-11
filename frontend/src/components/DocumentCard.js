b817d9d8595c930f77784d06ab004d62ecd43ff9
mskaa3 <j.moskaaa@gmail.com>
2022-11-11 14:04:40 +0100

    qa functionality working on the backend

---
+++ frontend/src/components/DocumentCard.js
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import { useEffect, useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import ListGroup from 'react-bootstrap/ListGroup';
+import {Col} from 'react-bootstrap'
+import DocumentCard from '../components/DocumentCard';
+
+const fun = () => {
+  return (
+  <div>    
+ 
+    <h1>component</h1>
+    
+    </div>  
+  );
+};
+export default fun;
