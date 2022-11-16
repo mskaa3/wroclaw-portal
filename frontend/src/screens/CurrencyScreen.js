87ad5b01d9caa2900f5a9fc372d8596326f78add
WhiteOwlhead <86147489+WhiteOwlhead@users.noreply.github.com>
2022-11-16 12:04:21 +0100

    scrapp + table

--- frontend/src/screens/CurrencyScreen.js
+++ frontend/src/screens/CurrencyScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import CurrencyCalculator from '../components/CurrencyCalculator';
+import CurrencyTable from '../components/CurrencyTable';
 
 const CurrencyScreen = () => {
   return (
     <div>
       <CurrencyCalculator></CurrencyCalculator>
+      <br></br>
+      <CurrencyTable></CurrencyTable>
     </div>
   );
 };
